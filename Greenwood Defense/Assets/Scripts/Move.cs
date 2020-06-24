e71dc9870bd7e454cbeb8c516b64991a42aef49c
TThanushan <thanushantharmabalan@gmail.com>
2020-06-24 02:07:28 +0200

    Healthbar, two units behaviors and animations
    Healthbar done, unit move and attack enemies with animation playing (bug occurs).

---
+++ Greenwood Defense/Assets/Scripts/Move.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour
+{
+    public float moveSpeedX = 1;
+    public float moveSpeedY = 0;
+
+    public int wayX = 1;
+    public int wayY = 1;
+
+    void Update()
+    {
+        transform.Translate(new Vector2(moveSpeedX * wayX * Time.deltaTime, moveSpeedY * wayY * Time.deltaTime));
+    }
+}
---
+++ Greenwood Defense/Assets/Scripts/UnitAnimatorManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitAnimatorManager : MonoBehaviour
+{
+
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        GetComponent<Unit>().onAttack += PlayAttackAnimation;
+        GetComponent<Unit>().OnDeath += PlayDeathAnimation;
+
+    }
+
+    private void Update()
+    {
+        string clipName = animator.GetCurrentAnimatorClipInfo(0)[0].clip.name;
+        if (!GetComponent<Unit>().Target && clipName != "Run" && clipName != "Attack")
+            animator.Play("Run");
+    }
+
+    private void PlayAttackAnimation()
+    {
+        animator.Play("Attack");
+    }
+
+    private void PlayDeathAnimation()
+    {
+        animator.Play("Death");
+    }
+}
